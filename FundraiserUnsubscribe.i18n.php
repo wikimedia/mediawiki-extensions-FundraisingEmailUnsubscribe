ac1578953c5cb21a366ae35b5ce55da34ea08467
paladox <thomasmulhall410@yahoo.com>
2014-06-15 23:18:07 -0700

    Fix JSON i18n
    Fix i18n file by removing /./ from i18n file.
    
    Change-Id: I668840b80fe3f136bbbb238bffea107bc6ce35d8

--- FundraiserUnsubscribe.i18n.php
+++ FundraiserUnsubscribe.i18n.php
@@ -15,7 +15,7 @@ if ( !function_exists( 'wfJsonI18nShim7d057ae20ecd550e' ) ) {
 	function wfJsonI18nShim7d057ae20ecd550e( $cache, $code, &$cachedData ) {
 		$codeSequence = array_merge( array( $code ), $cachedData['fallbackSequence'] );
 		foreach ( $codeSequence as $csCode ) {
-			$fileName = dirname( __FILE__ ) . "/./i18n/$csCode.json";
+			$fileName = dirname( __FILE__ ) . "/i18n/$csCode.json";
 			if ( is_readable( $fileName ) ) {
 				$data = FormatJson::decode( file_get_contents( $fileName ), true );
 				foreach ( array_keys( $data ) as $key ) {
