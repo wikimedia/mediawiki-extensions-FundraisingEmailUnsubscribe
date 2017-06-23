78a4bb46235fdba9be1c00a9b619e965eb6ded66
Umherirrender <umherirrender_de.wp@web.de>
2017-06-23 23:34:14 +0200

    Use short array syntax in alias file
    Change-Id: I28ccffd34e48f1856b55325c6840577aa18f9e1e

--- FundraisingEmailUnsubscribe.alias.php
+++ FundraisingEmailUnsubscribe.alias.php
@@ -5,11 +5,10 @@
  * @file
  * @ingroup Extensions
  */
-// @codingStandardsIgnoreFile
 
-$specialPageAliases = array();
+$specialPageAliases = [];
 
 /** English (English) */
-$specialPageAliases['en'] = array(
-	'FundraiserUnsubscribe' => array( 'FundraiserUnsubscribe' ),
-);
+$specialPageAliases['en'] = [
+	'FundraiserUnsubscribe' => [ 'FundraiserUnsubscribe' ],
+];
