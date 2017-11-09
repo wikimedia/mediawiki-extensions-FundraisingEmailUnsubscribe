370f552b8cd143f2f897787a022ec9f75789a68a
Umherirrender <umherirrender_de.wp@web.de>
2017-11-09 22:02:16 +0100

    Add stylelint for css file
    Change-Id: If6157a71a237f0128a02a2cffa65aa7f402685bf

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,7 @@ module.exports = function ( grunt ) {
 	grunt.loadNpmTasks( 'grunt-jsonlint' );
 	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
 	grunt.loadNpmTasks( 'grunt-banana-checker' );
+	grunt.loadNpmTasks( 'grunt-stylelint' );
 
 	grunt.initConfig( {
 		banana: {
@@ -21,9 +22,16 @@ module.exports = function ( grunt ) {
 				'!node_modules/**',
 				'!vendor/**'
 			]
+		},
+		stylelint: {
+			all: [
+				'**/*.css',
+				'!node_modules/**',
+				'!vendor/**'
+			]
 		}
 	} );
 
-	grunt.registerTask( 'test', [ 'jsonlint', 'banana', 'jshint' ] );
+	grunt.registerTask( 'test', [ 'jsonlint', 'stylelint', 'banana', 'jshint' ] );
 	grunt.registerTask( 'default', 'test' );
 };
