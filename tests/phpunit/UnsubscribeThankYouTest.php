1251b736d3d355403286e7cb7ea4f5bc30e73ea1
Elliott Eggleston <ejegg@ejegg.com>
2020-10-20 12:55:52 -0500

    Tests: autoload from extension-specific composer dir
    The extension.json load_composer_autoloader attribute SHOULD be
    doing this, but it doesn't seem to be working.
    
    Change-Id: I07a85f6d45723a3dc65eaa2ee2a9482d045ead2b

--- tests/phpunit/UnsubscribeThankYouTest.php
+++ tests/phpunit/UnsubscribeThankYouTest.php
@@ -1,5 +1,9 @@
 <?php
 
+if ( file_exists( __DIR__ . '/../../vendor/autoload.php' ) ) {
+	include_once( __DIR__ . '/../../vendor/autoload.php' );
+}
+
 /**
  * @group FundraisingEmailUnsubscribe
  */
