2fe31ede5f85fa99920021409dea31a056c6a73a
Umherirrender <umherirrender_de.wp@web.de>
2026-02-13 19:46:15 +0100

    tests: Remove separate include of vendor autoload.php
    This is done via extension.json setting: load_composer_autoloader
    
    Change-Id: I6fe22b504cff6f181ca22e84d99d02fcf417604b

--- tests/phpunit/SubscribeTest.php
+++ tests/phpunit/SubscribeTest.php
@@ -1,9 +1,5 @@
 <?php
 
-if ( file_exists( __DIR__ . '/../../vendor/autoload.php' ) ) {
-	include_once __DIR__ . '/../../vendor/autoload.php';
-}
-
 /**
  * @group FundraisingEmailUnsubscribe
  * @covers FundraiserSubscribe
